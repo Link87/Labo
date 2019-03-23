3ab2b4d3a8d92debdbcc793812acdc1e66ae2061
Marvin <m.gazibaric@live.de>
2019-03-23 23:56:42 +0100

    Move logic to library crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,41 @@
-use std::env;
+extern crate futures;
+extern crate telegram_bot_fork;
+extern crate tokio;
+
+use futures::{Stream, future::lazy};
+use telegram_bot_fork::*;
+use tokio::runtime::current_thread::Runtime;
 
 #[cfg(test)]
 mod tests;
+mod token;
+
+use token::TELEGRAM_BOT_TOKEN;
 
 #[derive(Debug)]
 pub struct Config {
-    pub api_token: String,
+    api_token: String,
     programs: Vec<Program>,
 }
 
 impl Config {
     pub fn new() -> Result<Config, ()> {
-        let api_token = match env::var("TELEGRAM_BOT_TOKEN") {
-            Ok(token) => token,
-            Err(_) => return Err(()),
-        };
         Ok(Config {
-            api_token,
+            //api_token,
+            api_token: String::from(TELEGRAM_BOT_TOKEN),
             programs: Vec::new(),
         })
     }
 }
 
 struct Washer {
+    state: WasherState,
+}
 
+enum WasherState {
+    Active(Program),
+    Finished(Program),
+    Idle,
 }
 
 #[derive(PartialEq, Debug)]
@@ -31,4 +43,44 @@ struct Program {
     name: String,
     temp: u8,
     duration: u16,
+}
+
+pub fn run_bot(config: Config) {
+    Runtime::new().unwrap().block_on(lazy(|| {
+        let api = Api::new(config.api_token).unwrap();
+
+        // Convert stream to the stream with errors in result
+        let stream = api.stream().then(|mb_update| {
+            let res: Result<Result<Update, Error>, ()> = Ok(mb_update);
+            res
+        });
+
+        // Print update or error for each update.
+        stream.for_each(move |update| {
+            match update {
+                Ok(update) => {
+                     // If the received update contains a new message...
+                    if let UpdateKind::Message(message) = update.kind {
+                        handle_message(&api, message);
+                    }
+                }
+                Err(err) => eprintln!("{}", err),
+            }
+
+            Ok(())
+        })
+    })).unwrap();
+}
+
+pub fn handle_message(api: &Api, message: Message) {
+    if let MessageKind::Text { ref data, .. } = message.kind {
+        // Print received text message to stdout.
+        println!("<{}>: {}", &message.from.first_name, data);
+
+        // Answer message with "Hi".
+        api.spawn(message.text_reply(format!(
+            "Hi, {}! You just wrote '{}'",
+            &message.from.first_name, data
+        )));
+    }
 }
